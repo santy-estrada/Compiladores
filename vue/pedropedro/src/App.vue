fba81901e7f874724ddad5d4fb402c5956e610e5
santy-estrada <santiago.estrada6@eia.edu.co>
2024-04-22 11:23:25 -0500

    Pedro Pedro

---
+++ vue/pedropedro/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <nav>
+    <li v-for="(meal, index) in food" v-bind:key="index">
+        {{ meal }}
+    </li>
+    <input type="text" name = "nombre" v-model = "name" placeholder="Qué otra comida quiere??" v-on:keydown.enter="addElement"/>
+    <br/>
+    <p>{{ name }}</p>
+    <h1>{{ counter }} <sup>2</sup> = {{ getSquareComputed }}</h1>
+    <h1>{{ counter }} <sup>2</sup> = {{ getSquareComputed }}</h1>
+    <h1>{{ counter }} <sup>2</sup> = {{ getSquareComputed }}</h1>
+
+  </nav>
+
+  <CounterApp title = "yey"/>
+  <CounterApp title = "NO yey"/>
+  
+</template>
+
+<script>
+import CounterApp from './components/CounterApp.vue'
+export default {
+  name: 'App',
+  components: {
+    CounterApp
+  },
+  data(){ //Atributos del componente
+    return{
+      food: ['Pizza', 'Hamburguesa', 'Carne'],
+      name:'',
+      counter:5
+    }
+  },
+  methods:{ //Métodos del componente
+    addElement(){
+      this.food.push(this.name);
+      this.name='';
+    },
+    getSquare(){
+      return this.counter*this.counter;
+    }
+  },
+  computed:{  //Métodos que se calculan sólo una vez
+    getSquareComputed (){
+      return this.counter*this.counter;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/pedropedro/src/components/CounterApp.vue
@@ -0,0 +1,40 @@
+<template>
+    <h1>{{ title || "Componente sin nombre" }}</h1>
+    <p>{{ counter }}</p>
+    <button @click="addOne">+1</button> <button @click="subOne">-1</button>
+</template>
+
+<script>
+export default{
+    props:['title'], 
+    data(){
+        return{
+            counter:0
+        }
+    },
+    methods:{
+        addOne(){
+            this.counter++;
+        },
+        subOne(){
+            this.counter--;
+        }
+    }
+}
+</script>
+
+<style>
+    button{
+        background-color: #64b664;
+        border-radius: 5px;
+        border: 1px solid white;
+        cursor: pointer;
+        margin: 0 5px;
+        padding: 5px 15px;
+        transition: 0.3 ease-in-out;
+    }
+    button:hover{
+        background-color: #6565b6;
+        transition: 0.3s ease-in-out;
+    }
+</style>
